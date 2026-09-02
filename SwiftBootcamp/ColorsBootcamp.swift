b7315a0a2bd8c96e10ec559fa0367f538aaedc93
abdullah-appnetic <abdullah.appnetic@gmail.com>
2026-09-02 16:29:49 +0500

    Colors

--- SwiftBootcamp/ColorsBootcamp.swift
+++ SwiftBootcamp/ColorsBootcamp.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ColorsBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 25, style: .circular)
+            .fill(.red)
+            .frame(width: 300, height: 200)
     }
 }
 
